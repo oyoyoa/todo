8402b1f6c513ec3be690095882f686c510999293
Takuma Iida <ta9maxxx@gmail.com>
2018-05-17 19:04:08 +0900

    init ios

---
+++ ios/todo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todo
+//
+//  Created by 飯田拓馬 on 2018/05/17.
+//  Copyright © 2018年 CATK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
